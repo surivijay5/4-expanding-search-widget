e3e50926adef3dda25631a5c7a92d6bd1a4459b8
Vijay Sai <surivijay5@gmail.com>
2021-09-18 09:55:22 +0530

    Add hidden search widget

---
+++ script.js
@@ -0,0 +1,7 @@
+const search = document.querySelector('.search')
+const button = document.querySelector('.btn')
+const input = document.querySelector('.input')
+
+button.addEventListener('click',() =>{
+    search.classList.toggle('active')
+})
